93b09ffaf83a915c381c16c1c552d44974c98838
Ahmad <muahmad710@gmail.com>
2022-10-23 19:03:28 +0500

    Commented - Josephus problem

--- Python/josephus_problem.py
+++ Python/josephus_problem.py
@@ -1,3 +1,5 @@
+# Josephus Problem - In a circle there are certain number of people standing, at k intervals the person present will be killed, people will be killed till only one os them is remaining. Write an algorithm that can find out the index of the person that will stay alive.  
+
 x = int(input("Enter number of elements: "))
 x += 1
 k = int(input("Enter k "))
