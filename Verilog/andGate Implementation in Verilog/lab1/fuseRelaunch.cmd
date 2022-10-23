382a6fb11e110b01f8700bf4427a662534fffb80
MUHAMMAD KALEEM ULLAH <92902603+fa19bce007@users.noreply.github.com>
2022-10-23 10:00:56 +0500

    Add files via upload

---
+++ Verilog/andGate Implementation in Verilog/lab1/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "E:/CE-7/Digital System Design - DSD/Lab/Xilinx Projects/lab1/andGateTest_isim_beh.exe" -prj "E:/CE-7/Digital System Design - DSD/Lab/Xilinx Projects/lab1/andGateTest_beh.prj" "work.andGateTest" "work.glbl" 
