ad8b0422d21f7136086e0f52c80d49ab8125b820
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-10-25 04:10:07 +0500

    Added pop Function

--- Web (HTML CSS JavaScript)/JavaScript Data Structures/Stacks.js
+++ Web (HTML CSS JavaScript)/JavaScript Data Structures/Stacks.js
@@ -18,6 +18,17 @@ Stack = function () {
     this.data[this.count] = value;
     this.count++;
   };
-  
+  this.pop = function () {
+    // if Condition to check if Stack is Empty if yes then return undefined
+    if (this.count == 0) {
+      return undefined;
+    }
+    // other wise decrease the count and return the value
+    this.count--;
+    var result = this.data[this.count];
+    delete this.data[this.count];
+    return result;
+  }
+  //  Checking Length/Size of Stack 
 
 }
